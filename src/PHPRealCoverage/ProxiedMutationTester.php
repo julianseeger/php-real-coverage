96517acb062e78f577d1b8bfff91f8c2c7aafd19
Julian <seeger.julian@gmail.com>
2014-03-07 00:18:13 +0100

    ProxiedMutataionTester loads class into proxy

--- src/PHPRealCoverage/ProxiedMutationTester.php
+++ src/PHPRealCoverage/ProxiedMutationTester.php
@@ -3,10 +3,28 @@
 namespace PHPRealCoverage;
 
 use PHPRealCoverage\Mutator\MutationTester;
+use PHPRealCoverage\Proxy\ClassMetadata;
+use PHPRealCoverage\Proxy\Proxy;
 
 class ProxiedMutationTester implements MutationTester
 {
+    /**
+     * @var Proxy
+     */
+    private $proxy;
+    /**
+     * @var ClassMetadata
+     */
+    private $class;
+
+    public function __construct(Proxy $proxy, ClassMetadata $class)
+    {
+        $this->class = $class;
+        $this->proxy = $proxy;
+    }
+
     public function isValid()
     {
+        $this->proxy->loadClass($this->class);
     }
 }
--- tests/PHPRealCoverage/ProxiedMutationTesterTest.php
+++ tests/PHPRealCoverage/ProxiedMutationTesterTest.php
@@ -4,8 +4,18 @@ namespace PHPRealCoverage;
 
 class ProxiedMutationTesterTest extends \PHPUnit_Framework_TestCase
 {
-    public function testA()
+    public function testTesterLoadsClassIntoProxy()
     {
-        $tester = new ProxiedMutationTester();
+        $class = $this->getMockForAbstractClass('PHPRealCoverage\Proxy\ClassMetadata');
+        $proxy = $this->getMockBuilder('PHPRealCoverage\Proxy\Proxy')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $proxy->expects($this->exactly(2))
+            ->method('loadClass')
+            ->with($class);
+
+        $tester = new ProxiedMutationTester($proxy, $class);
+        $tester->isValid();
+        $tester->isValid();
     }
 }
