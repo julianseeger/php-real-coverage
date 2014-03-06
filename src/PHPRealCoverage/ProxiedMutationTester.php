ade920f78f240ae10a37697c323627f364d03f3b
Julian <seeger.julian@gmail.com>
2014-03-06 23:47:24 +0100

    ProxiedMutator structure

---
+++ src/PHPRealCoverage/ProxiedMutationTester.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace PHPRealCoverage;
+
+use PHPRealCoverage\Mutator\MutationTester;
+
+class ProxiedMutationTester implements MutationTester
+{
+    public function isValid()
+    {
+    }
+}
---
+++ tests/PHPRealCoverage/ProxiedMutationTesterTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace PHPRealCoverage;
+
+class ProxiedMutationTesterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testA()
+    {
+        $tester = new ProxiedMutationTester();
+    }
+}
