e36fe81beefff7cdc55bbed25db6753a27ac4d32
Julian <seeger.julian@gmail.com>
2014-03-07 21:48:54 +0100

    added support to detect mutations with holes

--- src/PHPRealCoverage/Mutator/Mutator.php
+++ src/PHPRealCoverage/Mutator/Mutator.php
@@ -11,11 +11,15 @@ class Mutator
         try {
             while (true) {
                 $commands = $generator->getMutationStack();
-                foreach ($commands as $command) {
-                    $command->execute();
+                $lineToTest = $commands[0];
+                $lineToTest->execute();
+
+                if ($tester->isValid()) {
+                    continue;
                 }
 
-                if (!$tester->isValid()) {
+                $rest = array_slice($commands, 1);
+                if (!$this->findWorkingPermutation($rest, $tester)) {
                     foreach ($commands as $command) {
                         $command->undo();
                     }
@@ -23,5 +27,31 @@ class Mutator
             }
         } catch (NoMoreMutationsException $e) {
         }
+    }
+
+    /**
+     * @param MutationCommand[] $commands
+     * @param MutationTester $tester
+     * @return bool
+     */
+    private function findWorkingPermutation(array $commands, MutationTester $tester)
+    {
+        if (empty($commands)) {
+            return false;
+        }
+
+        $lineToTest = $commands[0];
+        $lineToTest->execute();
+        if ($tester->isValid()) {
+            return true;
+        }
+
+        $rest = array_slice($commands, 1);
+        if ($this->findWorkingPermutation($rest, $tester)) {
+            return true;
+        }
+
+        $lineToTest->undo();
+        return $this->findWorkingPermutation($rest, $tester);
     }
 }
--- tests/PHPRealCoverage/Mutator/MutatorTest.php
+++ tests/PHPRealCoverage/Mutator/MutatorTest.php
@@ -35,7 +35,7 @@ class MutatorTest extends \PHPUnit_Framework_TestCase
     }
 
     public function testMutatorMutatesClass()
-    {
+    { //TODO partial dos / undos are completely missing
         // arrange
         $line1 = new DefaultMutatableLine();
         $line2 = new DefaultMutatableLine();
@@ -60,5 +60,43 @@ class MutatorTest extends \PHPUnit_Framework_TestCase
         $this->assertFalse($line1->isEnabled());
         $this->assertFalse($line2->isEnabled());
         $this->assertTrue($line3->isEnabled());
+    }
+
+    public function testMutatorDetectsPermutationsWithWholes()
+    {
+        //arrange
+        $line1 = new DefaultMutatableLine(); //neccessary
+        $line2 = new DefaultMutatableLine();
+        $line3 = new DefaultMutatableLine(); //neccessary
+        $line4 = new DefaultMutatableLine();
+
+        $class = $this->getMockForAbstractClass('PHPRealCoverage\Mutator\MutatableClass');
+        $class->expects($this->any())
+            ->method('getMutatableLines')
+            ->will($this->returnValue(array($line1, $line2, $line3, $line4)));
+
+        $generator = new MutationGenerator($class);
+
+        //tester will only validate if everything is enabled or line1 AND line3 are disabled
+        $validatorCallback = function () use ($line1, $line2, $line3, $line4) {
+            if ($line1->isEnabled() && $line2->isEnabled() && $line3->isEnabled() && $line4->isEnabled()) {
+                return true;
+            }
+            return !$line1->isEnabled() && $line2->isEnabled() && !$line3->isEnabled() && $line4->isEnabled();
+        };
+        $tester = $this->getMockForAbstractClass('PHPRealCoverage\Mutator\MutationTester');
+        $tester->expects($this->any())
+            ->method('isValid')
+            ->will($this->returnCallback($validatorCallback));
+
+        //act
+        $mutator = new Mutator();
+        $mutator->testMutations($tester, $generator);
+
+        //assert
+        $this->assertFalse($line1->isEnabled());
+        $this->assertTrue($line2->isEnabled());
+        $this->assertFalse($line3->isEnabled());
+        $this->assertTrue($line4->isEnabled());
     }
 }
